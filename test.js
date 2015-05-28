09d5b0ac7edca3f6e530e42da9186639ee551803
Nicole Javorsky <nsj2110@barnard.edu>
2015-05-28 15:29:35 -0400

    adding poet files

---
+++ test.js
@@ -0,0 +1,14 @@
+var
+  express = require('express'),
+  app = express(),
+  Poet = require('poet');
+
+var poet = Poet(app, {
+  posts: './_posts/',
+  postsPerPage: 5,
+  metaFormat: 'json'
+});
+
+poet.init().then(function () {
+  // ready to go!
+});
